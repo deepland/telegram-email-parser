4fdf4a787c9230e121037e7b3606a5b5e387d05f
save14 <save999999@gmail.com>
2017-06-16 22:53:57 +0300

    Add parser

--- index.js
+++ index.js
@@ -13,6 +13,36 @@ bot.on('polling_error', (error) => {
 bot.onText(/\/start/, showHelp);
 bot.onText(/\/help/, showHelp);
 
+bot.onText(/\/url (.*)/, async (msg, match) => {
+  const document = await get(match[0]);
+  const emails = parse(document);
+
+  let str = '';
+
+  for (var email of emails) {
+    str += email + '\n';
+  }
+
+  bot.sendMessage(msg.chat.id, str);
+});
+
+function parse (str) {
+  let emails = [];
+  let regexp = /[\w\.]+@\w+\.[A-Za-z]+/g;
+
+  while (result = regexp.exec(str)) {
+    emails.push(result);
+  }
+}
+
+function get (url) {
+  try {
+    return requestp(url);
+  } catch (e) {
+    console.log(`${url}: ${e}`);
+  }
+}
+
 function showHelp (msg) {
   const str = 'Commands:\n/url <url>';
   bot.sendMessage(msg.chat.id, str);
