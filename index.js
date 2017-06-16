93c4012d24234eb25081bad542d4969c8e4a9cc4
save14 <save999999@gmail.com>
2017-06-16 19:26:34 +0300

    Add error handling & help

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const requestp = require('request-promise-native');
 const TelegramBot = require('node-telegram-bot-api');
 
+const settings = require('./settings.json');
+
 const bot = new TelegramBot(settings.token, {polling: true});
+
+bot.on('polling_error', (error) => {
+  console.log('' + error);
+  if (error.response.body.error_code === 401) process.exit(1);
+});
+
+bot.onText(/\/start/, showHelp);
+bot.onText(/\/help/, showHelp);
+
+function showHelp (msg) {
+  const str = 'Commands:\n/url <url>';
+  bot.sendMessage(msg.chat.id, str);
+}
