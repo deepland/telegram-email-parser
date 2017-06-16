db7256a5af1bc0e6d5d5d1cc019eb0c69b8a9107
save14 <save999999@gmail.com>
2017-06-16 21:48:28 +0300

    Heroku init

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const requestp = require('request-promise-native');
 const TelegramBot = require('node-telegram-bot-api');
 
-const settings = require('./settings.json');
+const bot = new TelegramBot(process.env.TOKEN, {
+  webHook: {
+    port: process.env.PORT
+  }
+});
 
-const bot = new TelegramBot(settings.token, {polling: true});
+// HEROKU_URL example: https://<app-name>.herokuapp.com:443
+bot.setWebHook(`${process.env.HEROKU_URL}/bot${bot.token}`);
 
 bot.on('polling_error', (error) => {
   console.log('' + error);
